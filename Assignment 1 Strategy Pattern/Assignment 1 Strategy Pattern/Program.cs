f9d09905ca8c372d26d3b893cfb4f1c7ed062923
Kristofer Veselits <kveselits@eagles.ewu.edu>
2019-01-12 23:12:26 -0800

    Basic framework

---
+++ Assignment 1 Strategy Pattern/Assignment 1 Strategy Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_Strategy_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
