594731a448e3e43fc2a5dda9e1637fe9d8c296e3
Kristofer Veselits <kveselits@eagles.ewu.edu>
2019-02-06 10:21:17 -0800

    fixed naming

---
+++ Assignment4.RefactorToFactory/Assignment4.RefactorToFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment4.RefactorToFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
